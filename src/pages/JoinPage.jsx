6c3d1ad42757fec722490c9b30a0141991346c5e
Jiyeon Kim <cleo0718@gmail.com>
2023-03-25 22:50:10 +0900

    가입페이지 CSS최종
    추후 컴포넌트 분리 필요

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -32,16 +32,55 @@ const JoinHeader = styled.div`
 `;
 
 const StyledForm = styled.form`
-  margin-top: 1em;
+  margin-top: 3rem;
   font-size: 26px;
   width: auto;
   height: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledUl = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  margin-top: 1rem;
+
+  li:first-child {
+    justify-content: flex-start;
+    gap: 1.2rem;
+  }
+
+  li:first-child input {
+    width: 45%;
+    margin-left: 70px;
+  }
+
+  li:nth-child(5) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  li:nth-child(6) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const StyledLi = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const StyledInput = styled.input`
   width: 460px;
   height: 54px;
-  margin: 1rem 5rem;
+  margin-left: 1rem;
   background: #ffffff;
   border: 2px solid #000000;
 `;
@@ -52,7 +91,7 @@ const StyledCheckBox = styled.input`
 `;
 
 const DuplicatedId = styled.button`
-  width: 215px;
+  width: 157px;
   height: 54px;
   background: #1c1c1c;
   border: 2px solid #000000;
@@ -61,7 +100,7 @@ const DuplicatedId = styled.button`
   font-style: normal;
   font-weight: 400;
   font-size: 24px;
-  line-height: 27px;
+  line-height: 30px;
   :hover {
     color: #939393;
   }
@@ -93,22 +132,33 @@ export default function JoinPage(props) {
           <span>SIGN UP</span>
         </JoinHeader>
         <StyledForm>
-          아이디 <StyledInput type="text" name="id"></StyledInput>
-          <DuplicatedId type="button">중복확인</DuplicatedId>
-          <br />
-          프로필명 <StyledInput type="text" name="profileName"></StyledInput>
-          <br />
-          패스워드 <StyledInput type="password" name="password"></StyledInput>
-          <br />
-          패스워드 확인{" "}
-          <StyledInput type="password" name="passwordCheck"></StyledInput>
-          <br />
-          <StyledCheckBox type="checkbox" id="joinCheck"></StyledCheckBox>
-          <label htmlFor="joinCheck">
-            가입 시 입력했던 정보는 탈퇴 시 소멸됩니다.
-          </label>
-          <br />
-          <JoinCheckButton type="submit">가입하기</JoinCheckButton>
+          <StyledUl>
+            <StyledLi>
+              아이디 <StyledInput type="text" name="id"></StyledInput>
+              <DuplicatedId type="button">중복확인</DuplicatedId>
+            </StyledLi>
+            <StyledLi>
+              프로필명{" "}
+              <StyledInput type="text" name="profileName"></StyledInput>
+            </StyledLi>
+            <StyledLi>
+              패스워드{" "}
+              <StyledInput type="password" name="password"></StyledInput>
+            </StyledLi>
+            <StyledLi>
+              패스워드 확인{" "}
+              <StyledInput type="password" name="passwordCheck"></StyledInput>
+            </StyledLi>
+            <StyledLi>
+              <StyledCheckBox type="checkbox" id="joinCheck"></StyledCheckBox>
+              <label htmlFor="joinCheck">
+                가입 시 입력했던 정보는 탈퇴 시 소멸됩니다.
+              </label>
+            </StyledLi>
+            <StyledLi>
+              <JoinCheckButton type="submit">가입하기</JoinCheckButton>
+            </StyledLi>
+          </StyledUl>
         </StyledForm>
       </Container>
     </Wrap>
