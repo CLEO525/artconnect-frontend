4e38bf3255b4b555aa3cf236258942d842f9ae6c
Jiyeon Kim <cleo0718@gmail.com>
2023-03-31 17:52:58 +0900

    로그인 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from "react";
-import styled from "styled-components";
-import JoinPage from "./pages/JoinPage";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import EditPage from "./pages/mypage/EditPage";
-import WritePage from "./pages/admin/WritePage";
-import BookmarkPage from "./pages/mypage/BookmarkPage";
-
+import Home from "./pages/Home";
 import "./App.css";
-
-const Wrap = styled.div`
-  width: auto;
-  height: auto;
-  margin: auto 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
 function App() {
   return (
-    <Wrap>
-      <JoinPage />
-      <LoginPage />
-      <EditPage />
-      <BookmarkPage />
-
-      {/* admin페이지 */}
-      <WritePage />
-    </Wrap>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
